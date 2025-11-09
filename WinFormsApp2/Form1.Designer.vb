da550d9258452de9b96e79db92151008bb8d3144
Lewis Orr Houston <wllhouston@glow.sch.uk>
2025-11-09 16:48:37 +0000

    Added Clikers

--- WinFormsApp2/Form1.Designer.vb
+++ WinFormsApp2/Form1.Designer.vb
@@ -28,24 +28,25 @@ Partial Class Form1
         Cookie = New Button()
         Label1 = New Label()
         Label2 = New Label()
+        Button1 = New Button()
+        Label3 = New Label()
+        TextBox1 = New TextBox()
         SuspendLayout()
         ' 
         ' Counter
         ' 
         Counter.Enabled = False
         Counter.ForeColor = Color.Black
-        Counter.Location = New Point(670, 60)
-        Counter.Margin = New Padding(3, 4, 3, 4)
+        Counter.Location = New Point(586, 45)
         Counter.Name = "Counter"
-        Counter.Size = New Size(62, 27)
+        Counter.Size = New Size(55, 23)
         Counter.TabIndex = 1
         ' 
         ' multplus
         ' 
-        multplus.Location = New Point(599, 99)
-        multplus.Margin = New Padding(3, 4, 3, 4)
+        multplus.Location = New Point(524, 74)
         multplus.Name = "multplus"
-        multplus.Size = New Size(119, 56)
+        multplus.Size = New Size(104, 42)
         multplus.TabIndex = 3
         multplus.Text = "+1 Multiplier Cost 100 cookies"
         multplus.UseVisualStyleBackColor = True
@@ -54,10 +55,9 @@ Partial Class Form1
         ' 
         MultCounter.Enabled = False
         MultCounter.ForeColor = Color.Black
-        MultCounter.Location = New Point(670, 163)
-        MultCounter.Margin = New Padding(3, 4, 3, 4)
+        MultCounter.Location = New Point(586, 122)
         MultCounter.Name = "MultCounter"
-        MultCounter.Size = New Size(62, 27)
+        MultCounter.Size = New Size(55, 23)
         MultCounter.TabIndex = 4
         ' 
         ' Cookie
@@ -69,43 +69,73 @@ Partial Class Form1
         Cookie.FlatStyle = FlatStyle.Flat
         Cookie.ForeColor = Color.Transparent
         Cookie.Image = My.Resources.Resources.PerfectCookie_removebg_preview
-        Cookie.Location = New Point(-3, 28)
+        Cookie.Location = New Point(-3, 21)
+        Cookie.Margin = New Padding(3, 2, 3, 2)
         Cookie.Name = "Cookie"
-        Cookie.Size = New Size(596, 628)
+        Cookie.Size = New Size(522, 471)
         Cookie.TabIndex = 6
         Cookie.UseVisualStyleBackColor = False
         ' 
         ' Label1
         ' 
         Label1.AutoSize = True
-        Label1.Location = New Point(598, 166)
+        Label1.Location = New Point(523, 124)
         Label1.Name = "Label1"
-        Label1.Size = New Size(76, 20)
+        Label1.Size = New Size(61, 15)
         Label1.TabIndex = 7
         Label1.Text = "Multiplier:"
         ' 
         ' Label2
         ' 
         Label2.AutoSize = True
-        Label2.Location = New Point(600, 63)
+        Label2.Location = New Point(525, 47)
         Label2.Name = "Label2"
-        Label2.Size = New Size(64, 20)
+        Label2.Size = New Size(52, 15)
         Label2.TabIndex = 8
         Label2.Text = "Cookies:"
         ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(523, 151)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(104, 42)
+        Button1.TabIndex = 10
+        Button1.Text = "+1 Clicker Cost 10 cookies"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(523, 207)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(51, 15)
+        Label3.TabIndex = 12
+        Label3.Text = "Clickers:"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Enabled = False
+        TextBox1.ForeColor = Color.Black
+        TextBox1.Location = New Point(586, 205)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(55, 23)
+        TextBox1.TabIndex = 11
+        ' 
         ' Form1
         ' 
-        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         BackgroundImage = My.Resources.Resources._320px_BgBlue
-        ClientSize = New Size(731, 724)
+        ClientSize = New Size(640, 543)
+        Controls.Add(Label3)
+        Controls.Add(TextBox1)
+        Controls.Add(Button1)
         Controls.Add(Label2)
         Controls.Add(Label1)
         Controls.Add(Cookie)
         Controls.Add(MultCounter)
         Controls.Add(multplus)
         Controls.Add(Counter)
-        Margin = New Padding(3, 4, 3, 4)
         Name = "Form1"
         Text = "Form1"
         ResumeLayout(False)
@@ -117,5 +147,8 @@ Partial Class Form1
     Friend WithEvents Cookie As Button
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox1 As TextBox
 
 End Class
--- WinFormsApp2/Form1.vb
+++ WinFormsApp2/Form1.vb
@@ -1,12 +1,18 @@
 ﻿Imports System.IO
 Public Class Form1
+    Private ClickerCostPath = "ClickerCost.txt"
+    Private ClickerPath = "Clickers.txt"
+    Private ClickCost = 10
+    Private running As Boolean = False
+    Private Clickers = 0
     Private MultCostPath = "MultCost.txt"
     Private MultCost = 100
     Private Multiplier = 1
     Private Cookies = 0
     Private SavePath As String = "cookies.txt"
     Private MultPath As String = "Multiplier.txt"
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    Private Async Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ' Load cookies
         If File.Exists(SavePath) Then
             Try
@@ -38,20 +44,51 @@ Public Class Form1
             End Try
             multplus.Text = "+1 Multiplier Cost " & MultCost
         End If
+
+        If File.Exists(ClickerPath) Then
+            Try
+                Dim text As String = File.ReadAllText(ClickerPath)
+                Clickers = Integer.Parse(text)
+            Catch
+                Clickers = 0
+            End Try
+            TextBox1.Text = Clickers.ToString()
+        End If
+
+        If File.Exists(ClickerCostPath) Then
+            Try
+                Dim text As String = File.ReadAllText(ClickerCostPath)
+                ClickCost = Integer.Parse(text)
+            Catch
+                ClickCost = 10
+            End Try
+            Button1.Text = "+1 Clicker Cost " & ClickCost
+        End If
+
+        ' 🔹 Start the background cookie generator when the form loads
+        If Not running Then
+            running = True
+            Do While running
+                Await Task.Delay(1000)
+                If Clickers > 0 Then
+                    Cookies += Clickers * Multiplier
+                    Counter.Text = Cookies.ToString()
+                End If
+            Loop
+        End If
     End Sub
+
     Private Sub Cookie_Click(sender As Object, e As EventArgs) Handles Cookie.Click
         Cookies += Multiplier
         Counter.Text = Cookies.ToString()
     End Sub
+
     Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
         File.WriteAllText(SavePath, Cookies.ToString())
         File.WriteAllText(MultPath, Multiplier.ToString())
         File.WriteAllText(MultCostPath, MultCost.ToString())
-    End Sub
-
-
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
-
+        File.WriteAllText(ClickerPath, Clickers.ToString())
+        File.WriteAllText(ClickerCostPath, ClickCost.ToString())
     End Sub
 
     Private Sub multplus_Click(sender As Object, e As EventArgs) Handles multplus.Click
@@ -60,8 +97,22 @@ Public Class Form1
             Cookies -= MultCost
             MultCost += 100
             multplus.Text = "+1 Multiplier Cost " & MultCost
+            Counter.Text = Cookies.ToString
+            MultCounter.Text = Multiplier.ToString
+        End If
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Cookies >= ClickCost Then
+            Cookies -= ClickCost
             Counter.Text = Cookies.ToString()
-            MultCounter.Text = Multiplier.ToString()
+            Clickers += 1
+            TextBox1.Text = Clickers.ToString()
+            ClickCost += 20
+            Button1.Text = "+1 Clicker Cost " & ClickCost
         End If
     End Sub
 End Class
